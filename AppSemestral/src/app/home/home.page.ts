ce44729d74c74d8f59a53f2a06862a13dad60852
aravegac <ara.vegac@duocuc.cl>
2024-09-06 14:00:07 -0400

    TodoList

--- AppSemestral/src/app/home/home.page.ts
+++ AppSemestral/src/app/home/home.page.ts
@@ -9,7 +9,33 @@ export class HomePage {
 
   constructor() {}
 
+  listaHaceres = [{
+    nomItem: 'Codear',
+    fecVencItem: '01-10-24',
+    prioridadItem: 'alto',
+    categoriaItem: 'Trabajo'
+  },
+  {
+    nomItem: 'Diseño',
+    fecVencItem: '10-28-24',
+    prioridadItem: 'bajo',
+    categoriaItem: 'Trabajo'
+  },
+  {
+    nomItem: 'Compras',
+    fecVencItem: '10-30-24',
+    prioridadItem: 'medio',
+    categoriaItem: 'Personal'
+  },
+  {
+    nomItem: 'Ejercicios',
+    fecVencItem: '10-25-24',
+    prioridadItem: 'alto',
+    categoriaItem: 'Personal'
+  }
+  ]
 
+  hoy: number = Date.now()
 }
 
 
