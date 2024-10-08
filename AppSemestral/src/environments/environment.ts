a5237efd41148bb5d0abb8c82e7341dd28c34fd7
r3xyzz <rexgamer152@gmail.com>
2024-10-08 14:39:58 -0300

    configuracion del enviromet con los   firebaseConfig:{     apiKey:     authDomain:     proyectId:     storageBucket:     messaginSenderId     appId:     Importes en el app module ts, y el json de google

--- AppSemestral/src/environments/environment.ts
+++ AppSemestral/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: 'AIzaSyCWg347KKeGdHil0ApvBlkSqDM3wJdk43Q',
+    authDomain: 'ara-et-prog-movil.firebaseapp.com',
+    proyectId: 'ara-et-prog-movil',
+    storageBucket:'ara-et-prog-movil.appspot.com',
+    messaginSenderId:'453591658121',
+    appId:'1:453591658121:android:1c1e41205c57ec8c8f6058'
+  }
 };
 
 /*
