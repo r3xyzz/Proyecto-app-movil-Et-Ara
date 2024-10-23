abeff32ef2c80b98f5f2370c202a26a025708a73
aravegac <ara.vegac@duocuc.cl>
2024-10-22 22:49:11 -0300

    Mensajes de error o de validación hechas :-D

--- AppSemestral/src/app/paginas/registro/registro.page.ts
+++ AppSemestral/src/app/paginas/registro/registro.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseLoginService } from 'src/app/servicios/firebase-login.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -15,15 +16,61 @@ export class RegistroPage implements OnInit {
 
 
 
-  constructor(private acsses:FirebaseLoginService, router:Router) { 
+  constructor(private acsses:FirebaseLoginService, public router:Router,public alerta:AlertController) { 
 
   }
+
+  traducirMensajeError(error: any): string {
+    switch (error.code) {
+      case 'auth/email-already-in-use':
+        return 'Este correo ya está en uso.';
+      case 'auth/invalid-email':
+        return 'El formato del correo electrónico es incorrecto.';
+      case 'auth/weak-password':
+        return 'La contraseña es demasiado débil. Debe tener al menos 6 caracteres.';
+      default:
+        return 'Ocurrió un error inesperado. Por favor, inténtelo de nuevo.';
+    }
+  }
+  
+
+  async MensajeError(mensajeError: string) {
+    const alert = await this.alerta.create({
+      header: 'ERROR',
+      subHeader: 'Registro fallido',
+      message: mensajeError,
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+    }
+
+  async MensajeCorrecto() {
+      const alert = await this.alerta.create({
+        header: '¡ÉXITO!',
+        subHeader: 'Registro Exitoso',
+        message: 'Puedes Iniciar Sesión :-D',
+        buttons: [{
+          text: 'Dirigeme Allí',
+          handler: () => {
+            this.router.navigate(['/login']);  // Redirige a la página de login
+          }}]
+      });
+      await alert.present();
+      }
 
   ngOnInit() { }
 
 
   async crearUsuario(){
-    await this.acsses.create_user(this.password,this.nombre);
+    try{
+      await this.acsses.create_user(this.password,this.nombre);
+      console.log("Usuario creado exitosamente");
+      this.MensajeCorrecto();
+    }catch(error){
+      console.log("ERROR EN REGISTRO!!!!!!!!!!!!!!!")
+      this.MensajeError(this.traducirMensajeError(error));
+    }
+    
   }
 
 }
