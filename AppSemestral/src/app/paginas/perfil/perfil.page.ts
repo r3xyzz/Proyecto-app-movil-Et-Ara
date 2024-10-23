a028fe4fde5e0a9246f729d5f000478959503c83
r3xyzz <rexgamer152@gmail.com>
2024-10-23 20:00:07 -0300

    nueva funcion de cambio de nombre, Campo de entrada para cambiar el nombre, con su funcion de input de nombre, tu nombre y el async de guardado  (reutilizado del login)

--- AppSemestral/src/app/paginas/perfil/perfil.page.ts
+++ AppSemestral/src/app/paginas/perfil/perfil.page.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage {
+export class PerfilPage implements OnInit {
   currentDate: string;
+  nombre: string = "";
 
-  constructor() {
+  constructor(private storage: Storage) {
     const today = new Date();
     this.currentDate = today.toLocaleDateString();
+  }
+
+  async ngOnInit() {
+    await this.storage.create();
+    this.nombre = await this.storage.get('nombre') || 'Tu Nombre';
+  }
+
+  async guardarNombre() {
+    if (this.nombre === "") {
+      console.log("ERROR NOMBRE VACIO");
+      // muestrar un mensaje de error similar a MensajeError en login.page.ts
+    } else {
+      await this.storage.set('nombre', this.nombre);
+      console.log("Nombre guardado exitosamente");
+      // muestrar un mensaje de éxito similar a MensajeCorrecto en login.page.ts
+    }
   }
 }
