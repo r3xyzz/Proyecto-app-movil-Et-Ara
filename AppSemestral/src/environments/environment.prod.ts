fa1fdf569043fd6349f3f17336dfa0c8bb0d1f39
aravegac <ara.vegac@duocuc.cl>
2024-10-24 21:56:55 -0300

    implementación https://www.npmjs.com/package/@capacitor-community/speech-recognition

--- AppSemestral/src/environments/environment.prod.ts
+++ AppSemestral/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: 'AIzaSyCWg347KKeGdHil0ApvBlkSqDM3wJdk43Q',
+    authDomain: 'ara-et-prog-movil.firebaseapp.com',
+    projectId: 'ara-et-prog-movil',
+    storageBucket:'ara-et-prog-movil.appspot.com',
+    messaginSenderId:'453591658121',
+    appId:'1:453591658121:android:1c1e41205c57ec8c8f6058',
+  },
 };
